d99f58a9d7e006a2218e38608f15c381608c0c60
Misha Yatsenko <1234asdfm@gmail.com>
2018-11-14 11:08:42 +0200

    menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,7 @@ $(function () {
     $('ul.global-side-menu-list li').each(function() {
         if ($(this).has('ul').length) {
             $(this).addClass('ddown');
+            $(this).find("> a").after( '<span class="nav-direction"><i aria-hidden="true" class="fa fa-caret-down"></i></span>' );
         }
     });
 });
