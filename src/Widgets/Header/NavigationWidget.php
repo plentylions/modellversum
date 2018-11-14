7f63d94a27e39baa42002bacd17f6e4ae4fe2c66
Misha Yatsenko <1234asdfm@gmail.com>
2018-11-14 16:45:17 +0200

    header widget

---
+++ src/Widgets/Header/NavigationWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modellversum\Widgets\Header;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class NavigationWidget extends BaseWidget
+{
+    protected $template = "Modellversum::Widgets.Header.NavigationWidget";
+}
